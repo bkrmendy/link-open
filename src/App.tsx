e0080ca3b81bb0d08a756ef695079b8bb4de5774
Berci Kormendy <korber314@icloud.com>
2022-06-27 16:21:17 +0200

    poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function makeFauxLink(url: string): HTMLAnchorElement {
+  const element = document.createElement("a");
+  element.setAttribute("href", url);
+  element.setAttribute("target", "_blank");
+  element.style.display = "none";
+  document.body.appendChild(element);
+  return element;
+}
+
+function activateFauxLink(link: HTMLAnchorElement): void {
+  link.dispatchEvent(new MouseEvent("click"));
+  document.body.removeChild(link);
+}
+
+function clickUrl(url: string) {
+  const element = makeFauxLink(url);
+  setTimeout(() => {
+    activateFauxLink(element);
+  }, 1);
+}
 
 function App() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const query = params.get("link");
+    if (!query) {
+      return;
+    }
+
+    params.set("link", "");
+
+    clickUrl(decodeURIComponent(query));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
